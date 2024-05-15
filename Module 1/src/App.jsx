a55070f1a7a754d5959b6a16033bbe231acf0bfd
Vignesh Ramesh <r.vignesh149@gmail.com>
2024-05-15 18:57:29 +0530

    Finsihed module 1 react

--- Module 1/src/App.jsx
+++ Module 1/src/App.jsx
@@ -1,9 +1,15 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx"
 import CoreConcept from "./components/CoreConcepts.jsx"
-
+import TabButton from "./components/TabButton.jsx"
+import {useState} from "react"
 
 function App() {
+  let [selectedContent, setselectedContent] =  useState("");
+
+  function handleClick(selectedButton){
+    setselectedContent(selectedButton)
+  }
   return (
     <div>
       <Header />
@@ -11,9 +17,28 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            { CORE_CONCEPTS.map(concept => <CoreConcept {...concept}/> )}
+            { CORE_CONCEPTS.map(concept => <CoreConcept key={concept.title} {...concept}/> )}
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <li><TabButton onSelect={()=>handleClick("components")} isSelected={selectedContent==="components"}>Components</TabButton></li>
+            <li><TabButton onSelect={()=>handleClick("jsx")} isSelected={selectedContent==="jsx"}>JSX</TabButton></li>
+            <li><TabButton onSelect={()=>handleClick("props")} isSelected={selectedContent==="props"}>Props</TabButton></li>
+            <li><TabButton onSelect={()=>handleClick("state")} isSelected={selectedContent==="state"}>State</TabButton></li>
+
+          </menu>
+          {selectedContent ? (
+            <div id="tab-content">
+            <h3>{EXAMPLES[selectedContent].title}</h3>
+            <p>{EXAMPLES[selectedContent].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedContent].code}</code>
+            </pre>
+          </div>
+          ) : <p> Please select a topic. </p>}
+        </section>     
       </main>
     </div>
   );
